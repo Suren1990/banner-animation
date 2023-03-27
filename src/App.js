af0251af89a47ba0eae2dd3502ff3df1f52566d7
Suren1990 <suren.simonyan1990@gmail.com>
2023-03-28 00:57:20 +0400

    Add project to github

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import { images } from "./assets/images";
+import "./style.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="banner">
+      <div className="banner__top">
+        <div className="banner__image_wrap">
+          <img
+            src={images.Banner}
+            srcSet={`${images.Banner2x} 2x`}
+            className="banner__image"
+            alt="banner"
+          />
+        </div>
+        <div className="banner__top_info">
+          <div className="banner__top_info_inner">
+            <div className="banner__text_wrapper">
+              <p className="banner__top_text banner__text_animation">
+                new collection
+              </p>
+            </div>
+            <div className="banner__title_wrapper">
+              <h1 className="banner__top_title banner__text_animation">
+                Eget Tortor,
+              </h1>
+            </div>
+            <div className="banner__title_wrapper">
+              <h2 className="banner__top_title banner__text_animation">
+                Mollis Habitasse.
+              </h2>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="banner__bottom">
+        <div className="banner__bottom_inner">
+          <div className="banner__text_wrapper banner__mb1">
+            <p className="banner__bottom_text banner__bottom_animation">sale up to 50%</p>
+          </div>
+          <div className="banner__title_wrapper">
+            <h2 className="banner__bottom_title banner__bottom_animation">
+              Pulvinar Amet Morbi
+            </h2>
+          </div>
+          <div className="banner__title_wrapper">
+            <h2 className="banner__bottom_title banner__bottom_animation">
+              Efficitur Justo
+            </h2>
+          </div>
+          <div className="banner__model_wrap">
+            <img
+              src={images.Model}
+              srcSet={`${images.Model2x} 2x`}
+              className="banner__model_image"
+              alt="model"
+            />
+          </div>
+          <p className="banner__bottom_description">
+            Nunc venenatis adipiscing mauris lorem non risus molestie ut. Dolor
+            ex. Cras sodales efficitur nec
+          </p>
+          <a href="/#" className="banner__shop">
+            Shop All
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/assets/images.js
@@ -0,0 +1,11 @@
+import Banner from "./images/banner.jpg";
+import Banner2x from "./images/banner2x.jpg";
+import Model from "./images/model.jpg";
+import Model2x from "./images/model2x.jpg";
+
+export const images = {
+  Banner,
+  Banner2x,
+  Model,
+  Model2x,
+};
